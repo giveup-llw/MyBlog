2f594e44495b1476105fe788efc27893ae0386d9
liuliwei <1296548804@qq.com>
2019-12-15 12:21:21 +0800

    ydlxiug

--- blog1/views.py
+++ blog1/views.py
@@ -13,13 +13,12 @@ def index(request):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    post.increase_view()
+    post.increase_views()
     md=markdown.Markdown(extensions=[
         'markdown.extensions.extra',
         'markdown.extensions.codehilite',
-        'markdown.extensions.toc',
         TocExtension(slugify=slugify),
-                                  ])
+        ])
     post.body = md.convert(post.body)
     m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
     post.toc = m.group(1) if m is not None else ''
